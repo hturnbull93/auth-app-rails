32bdba0c3cc35f09200f314e337575f4d9ff4d56
Harry Turnbull <hturnbull@live.co.uk>
2020-05-16 00:18:21 +0100

    renders @current_user, else logged in false

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,7 +21,11 @@ class SessionsController < ApplicationController
     if @current_user
       render json: {
         logged_in: true,
-        user: user
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false,
       }
     end
   end
